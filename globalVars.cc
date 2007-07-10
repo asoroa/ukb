9e4972c27a621ae5cae75a46a00e6c4f0ed73821
ccpsoeta <ccpsoeta@41fde781-4c32-0410-9b47-839176a67e93>
2007-07-10 07:14:11 +0000

    + Global variables
    git-svn-id: svn+ssh://siuc04/sc02a1/users/ccpsoeta/svnRep/ukb/Main@15 41fde781-4c32-0410-9b47-839176a67e93

---
+++ globalVars.cc
@@ -0,0 +1,111 @@
+#include "globalVars.h"
+#include "common.h"
+#include <iostream>
+
+using namespace glVars;
+
+namespace glVars {
+  int verbose = 0;
+
+  std::vector<std::string> rel_source;
+  std::string w2s_filename = "mcr_source/enWN16";
+
+//   bool word_norep = 0;
+//   int hub_neighborhood = 0;
+//   float context_threshold = 0;
+
+//   bool FeaturesCoocurrence = true;
+//   size_t numHubsFixed = 0;
+
+//   VertexRelate vRelate = freq;
+//   HubsAlg hubsAlg = veronis;
+
+//   size_t Param::edgeFreqMin = 5;
+//   size_t Param::vertexFreqMin = 10;
+//   float  Param::ertzPisuMin = 0.1;
+//   size_t Param::ctxSizeMin = 4;
+
+//   size_t VeronisParam::hubNeighborSize = 6;
+//   float  VeronisParam::hubNeighborWeightMin = 0.2;
+//   float  VeronisParam::hubFreqThreshold = 0.001;
+
+//   float  PrankParam::freqThreshold = 0.002; // 
+//   int    PrankParam::siblingsCannotBeHubs = 0;
+
+//   float MclParam::Inflate = 0.0f;
+}
+
+void show_global_variables(std::ostream & o) {
+
+  o << "General options" << std::endl << "******" << std::endl;
+  o << "verbose:" << verbose << '\t' ;
+  o << "Rels: ";
+  writeV(o, rel_source);
+  o << std::endl;
+//   o <<  "Hub_neighborhood: " << hub_neighborhood << '\t';
+//   //o << "Context_threshold: " << context_threshold << '\t';
+//   o << "FeaturesCoocurrence: " << FeaturesCoocurrence << '\t';
+//   o << "word_norep: " << word_norep << std::endl;
+
+//   o << "Hub algorithm: ";
+
+//   switch (hubsAlg) {
+//   case akOrok::veronis:
+//     o << "veronis";
+//     break;
+//   case akOrok::pageRank:
+//     o << "pageRank";
+//     break;
+//   case akOrok::degree:
+//     o << "degree";
+//     break;
+//   case akOrok::hits:
+//     o << "hits";
+//     break;
+//   case akOrok::mcl:
+//     o << "mcl";
+//     break;
+//   case akOrok::random:
+//     o << "random";
+//     break;
+//   default:
+//     o << "unknow (ERROR)";
+//   }
+//   if ((hubsAlg == akOrok::pageRank) || (hubsAlg == akOrok::hits)) {
+//     o << '\t' << "Stop Criterion: ";
+//     if (akOrok::numHubsFixed) o << "Fixed num hubs";
+//     else o << "Freq. threshold";
+//   }
+//   o << std::endl;
+//   o << "Relatedness: ";
+//   switch (vRelate) {
+//   case akOrok::freq:
+//     o << "freq";
+//     break;
+//   case akOrok::chsq:
+//     o << "chsq";
+//     break;
+//   default:
+//     o << "unknow (ERROR)";
+//   }
+//   o << std::endl;
+//   o << "Param::edgeFreqMin: " << Param::edgeFreqMin << '\t';
+//   o << "Param::vertexFreqMin: " << Param::vertexFreqMin << std::endl;
+//   o << "Param::ertzPisuMin: " << Param::ertzPisuMin << '\t';
+//   o << "Param::ctxSizeMin: " << Param::ctxSizeMin << std::endl;
+//   if (akOrok::numHubsFixed) {
+//     o << "akOrok::numHubsFixed: " << akOrok::numHubsFixed << std::endl;
+//   } else {
+//     if (hubsAlg == veronis) {
+//       o << "VeronisParam::hubNeighborSize: " << VeronisParam::hubNeighborSize << '\t';
+//       o << "VeronisParam::hubNeighborWeightMin: " << VeronisParam::hubNeighborWeightMin << std::endl;
+//       o << "VeronisParam::hubFreqThreshold: " << VeronisParam::hubFreqThreshold << std::endl;
+//     } else {
+//       o << "PrankParam::freqThreshold: " << PrankParam::freqThreshold << '\t';
+//       o << "PrankParam::siblingsCannotBeHubs: " << PrankParam::siblingsCannotBeHubs << std::endl;
+//     }
+//   }
+//   if (hubsAlg == mcl) {
+//     o << "MclParam::Inflate: " << MclParam::Inflate << std::endl;
+//   }
+}
---
+++ globalVars.h
@@ -0,0 +1,79 @@
+// -*-C++-*-
+
+#ifndef GLVARS_H
+#define GLVARS_H
+
+#include<iosfwd>
+#include<cstddef>
+#include<string>
+#include<vector>
+
+namespace glVars {
+  // debug
+  extern int verbose;
+
+  extern std::vector<std::string> rel_source;
+  extern std::string w2s_filename;
+
+  // Maiztasunak
+
+  // Ezaugarriak
+
+//   enum VertexRelate { // How to compute relateness between vertices in cooc. graph
+//     freq,
+//     chsq,         // Chi square
+//     rel_error
+//   };
+
+//  extern VertexRelate vRelate;
+
+//   enum HubsAlg {
+//     veronis,
+//     pageRank,
+//     hits,
+//     degree,
+//     mcl,
+//     random
+//   };
+
+//   extern HubsAlg hubsAlg;
+
+  
+//   struct Param {
+//     // coocurrences with a frequency of 5 or more are retained
+//     static size_t edgeFreqMin;
+
+//     // Words with less than 10 occurrences are discarded
+
+//     static size_t vertexFreqMin;
+
+//     // Edges with a weigth below 0.1 are arbitrarily (totally) elminiated
+//     static float ertzPisuMin;
+
+//     // Context containing fewer than 4 words are discarded
+//     static size_t ctxSizeMin;
+//   };
+
+//   struct VeronisParam {
+//     // Good candidate for Hub:
+//     // The mean of the weights btw. the candidate node and its 6 most frequent
+//     // neighbors must be below 0.8
+//     static size_t hubNeighborSize;
+//     static float hubNeighborWeightMin;
+//     static float hubFreqThreshold;
+//   };
+
+//   struct PrankParam {
+//     static float   freqThreshold;
+//     static int     siblingsCannotBeHubs;
+//   };
+
+//   struct MclParam {
+//     static float Inflate;
+//   };
+
+};
+
+void show_global_variables (std::ostream & o);
+
+#endif
