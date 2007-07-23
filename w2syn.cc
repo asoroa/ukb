3d2176f856622f96794e4db51e8eb7938003c772
ccpsoeta <ccpsoeta@41fde781-4c32-0410-9b47-839176a67e93>
2007-07-23 10:28:32 +0000

    + Add freqs managing (for calculating PPVs in pageRank)   	+ freqs are taken for WNet
    git-svn-id: svn+ssh://siuc04/sc02a1/users/ccpsoeta/svnRep/ukb/Main@20 41fde781-4c32-0410-9b47-839176a67e93

--- w2syn.cc
+++ w2syn.cc
@@ -1,6 +1,7 @@
 
 #include "w2syn.h"
 #include "globalVars.h"
+#include "common.h"
 
 #include <fstream>
 #include <iostream>
@@ -18,12 +19,26 @@ using namespace boost;
 // Global variables
 //const std::string w2s_filename = "mcr_source/enWN16";
 
+std::ostream & operator<<(std::ostream & o, const W2Syn_item & item) {
+  o << "S: ";
+  writeV(o, item.wsyns);
+  o << "\n";
+  if (item.has_freq) {
+    o << "F: ";
+    writeV(o, item.syns_count);
+  } else {
+    o << "No freqs";
+  }
+  o << endl;
+  return o;
+};
+
 
 ////////////////////////////////////////////////
 // Word2Synset stuff
 
 void read_w2syn_file(const string & fname,
-		     map<string, vector<string> > & w2syns) {
+		     map<string, W2Syn_item > & w2syns) {
     
   // optimize IO
   std::ios::sync_with_stdio(false);
@@ -53,15 +68,27 @@ void read_w2syn_file(const string & fname,
     vector<string>::const_iterator fields_it = fields.begin();
     vector<string>::const_iterator fields_end = fields.end();
     ++fields_it;
-    map<string, vector<string> >::iterator map_value_it;
-    tie(map_value_it, insertedP) = w2syns.insert(make_pair(fields[0], vector<string>(fields.size() - 1)));
-    if (!insertedP) {
-      //cerr << "read_w2syn_file warning in line: " << line_number << " " << fields[0] << " element is repeated." <<endl;
-      for(;fields_it != fields_end; ++fields_it) 
-	map_value_it->second.push_back(*fields_it);
-    } else {
-      copy(fields_it, fields_end, map_value_it->second.begin());
-    }
+    map<string, W2Syn_item >::iterator map_value_it;
+    tie(map_value_it, insertedP) = w2syns.insert(make_pair(fields[0], W2Syn_item()));
+    W2Syn_item & item = map_value_it->second;
+
+    for(; fields_it != fields_end; ++fields_it) {
+      char_separator<char> sf_sep(":");
+      tokenizer<char_separator<char> > sf_tok(*fields_it, sf_sep);
+      vector<string> syn_freq;
+      copy(sf_tok.begin(), sf_tok.end(), back_inserter(syn_freq));
+      if (syn_freq.size() == 0) {
+	cerr << "read_w2syn_file error. Bad line: " << line_number << endl;
+	exit(-1);
+      }
+      item.wsyns.push_back(syn_freq[0]);
+      if (syn_freq.size() > 1) {
+	item.has_freq = true;
+	item.syns_count.push_back(lexical_cast<size_t>(syn_freq[1]));
+      } else {
+	item.has_freq = false;
+      }
+    }    
   }
 }
   
@@ -74,23 +101,33 @@ W2Syn & W2Syn::instance() {
   return inst;
 }
 
-const vector<std::string> & W2Syn::get_syns(const std::string & word) const {
-  static vector<string> nullVector;
-  map<string, vector<string> >::const_iterator map_value_it = m_w2syns.find(word);
-  if (map_value_it == m_w2syns.end()) return nullVector; // null
-  return map_value_it->second;
+std::pair<vector<std::string>::const_iterator, vector<std::string>::const_iterator>
+W2Syn::get_wsyns(const std::string & word) const {
+  vector<string>::const_iterator null_it;
+  map<string, W2Syn_item >::const_iterator map_value_it = m_w2syns.find(word);
+  if (map_value_it == m_w2syns.end()) return make_pair(null_it, null_it); // null
+  return make_pair(map_value_it->second.wsyns.begin(),map_value_it->second.wsyns.end());
 }
 
-void W2Syn::w2syn_reverse(map<string, string> & rev) const {
+bool W2Syn::syn_counts(map<string, size_t> & res) const {
 
-  map<string, string>().swap(rev); // empty rev
+  map<string, size_t>().swap(res); // empty res
 
-  map<string, vector<string> >::const_iterator m_it = m_w2syns.begin();
-  map<string, vector<string> >::const_iterator m_end = m_w2syns.end();
+  map<string, W2Syn_item>::const_iterator m_it = m_w2syns.begin();
+  map<string, W2Syn_item>::const_iterator m_end = m_w2syns.end();
   for(;m_it != m_end; ++m_it) {
-    vector<string>::const_iterator v_it = m_it->second.begin();
-    vector<string>::const_iterator v_end =  m_it->second.end();
-    for(; v_it != v_end; ++v_it)
-      rev.insert(make_pair(*v_it, m_it->first));
+    const W2Syn_item & item = m_it->second;
+    if (!item.has_freq) return false;
+    assert(item.wsyns.size() == item.syns_count.size());
+    size_t m = item.wsyns.size();
+    for(size_t i = 0; i != m; ++i) {
+      bool insertedP;
+      map<string, size_t>::iterator map_it;
+      tie(map_it, insertedP) = res.insert(make_pair(item.wsyns[i], item.syns_count[i]));
+      if(!insertedP) {
+	map_it->second += item.syns_count[i];
+      }
+    }
   }
+  return true;
 }
--- w2syn.h
+++ w2syn.h
@@ -7,7 +7,7 @@
 #include <iterator>
 #include <map>
 #include <vector>
-#include <fstream>
+#include <iosfwd>
 
 // Global variables
 
@@ -15,18 +15,28 @@ extern const std::string w2s_filename;
 
 // Word2Syns stuff (singleton)
 
+
+struct W2Syn_item {
+  std::vector<std::string> wsyns;
+  std::vector<size_t> syns_count;
+  bool has_freq;
+};
+
+std::ostream & operator<<(std::ostream & o, const W2Syn_item & item);
+
 class W2Syn {
 public:
   // Singleton
   static W2Syn & instance();
 
-  const std::vector<std::string> & get_syns(const std::string & word) const;
-  void w2syn_reverse(std::map<std::string, std::string> & rev) const;
+  std::pair<std::vector<std::string>::const_iterator, std::vector<std::string>::const_iterator> 
+  get_wsyns(const std::string & word) const;
+  bool syn_counts(std::map<std::string, size_t> & res) const;
 private:
   W2Syn();
   W2Syn(const W2Syn &);
   W2Syn & operator=(const W2Syn &);
-  std::map<std::string, std::vector<std::string> > m_w2syns;
+  std::map<std::string, W2Syn_item > m_w2syns;
 };
 
 #endif
