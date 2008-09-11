asoroa/ukb

commit efeed4cbfd546ce36f3f81ea7c638774fbbf5fb0

Don't print extra space in keyfiles

csentence.cc
@@ -213,7 +213,7 @@ ostream & CWord::print_cword_aw(ostream & o) const {
 
   vector<string> id_fields(split(m_id, "."));
   assert(id_fields.size() > 0);
-  o << id_fields[0] << " " << m_id << " ";
+  o << id_fields[0] << " " << m_id;
   if(!glVars::output::allranks) cw_aw_print_best(o, m_syns, m_ranks);
   else cw_aw_print_all(o, m_syns, m_ranks);
   o << " !! " << w << "\n";
@@ -226,7 +226,7 @@ ostream & CWord::print_cword_semcor_aw(ostream & o) const {
 
   vector<string> id_fields(split(m_id, "."));
   assert(id_fields.size() > 0);
-  o << id_fields[0] << "." << id_fields[1] << " " << m_id << " ";
+  o << id_fields[0] << "." << id_fields[1] << " " << m_id;
   if(!glVars::output::allranks) cw_aw_print_best(o, m_syns, m_ranks);
   else cw_aw_print_all(o, m_syns, m_ranks);
   o << " !! " << w << "\n";

commit 58db3fd6cee67e69f909bd1f38abe7dd7489e514

Fix bug introduced in efeed4cbfd546ce36f3f81ea7c638774fbbf5fb0

csentence.cc
@@ -187,7 +187,7 @@ ostream & cw_aw_print_best(ostream & o,
 						   const vector<string> & syns,
 						   const vector<float> & ranks) {
   size_t n = syns.size();
-  o << syns[0];
+  o << " " << syns[0];
   for(size_t i = 1; i != n; ++i) {
     if (ranks[i] != ranks[0]) break;
     o << " " << syns[i];