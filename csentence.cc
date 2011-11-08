asoroa/ukb

commit 501d414e4a23a345d802126b3f7bde3e0403fc45

Add 'minput' commend line switch to deal with malformed input.

csentence.cc
@@ -72,7 +72,7 @@ namespace ukb {
 		new_c++;
 	  } else {
 		if (glVars::debug::warning) {
-		  cerr << "W:CWord: synset " << syn_str << " of word " << lemma << " is not in KB" << endl;
+		  cerr << "CWord:link_dict_concepts: concept " << syn_str << " of word " << lemma << " is not in KB" << endl;
 		}
 	  }
 	}
@@ -80,9 +80,9 @@ namespace ukb {
 	if (new_c == 0) return 0;
 
 	if(glVars::dict::use_shuffle) {
-		// Shuffle synsets string vector
-		boost::random_number_generator<boost::mt19937, long int> rand_dist(glVars::rand_generator);
-		std::random_shuffle(m_syns.begin(), m_syns.end(), rand_dist);
+	  // Shuffle synsets string vector
+	  boost::random_number_generator<boost::mt19937, long int> rand_dist(glVars::rand_generator);
+	  std::random_shuffle(m_syns.begin(), m_syns.end(), rand_dist);
 	}
 
 	// Update ranks
@@ -390,60 +390,66 @@ namespace ukb {
 
   istream & CSentence::read_aw(istream & is, size_t & l_n) {
 
-  string line;
+	string line;
 
-  if(read_line_noblank(is, line, l_n)) {
+	if(read_line_noblank(is, line, l_n)) {
 
-	// first line is id
-	char_separator<char> sep(" \t");
-	vector<string> ctx;
+	  // first line is id
+	  char_separator<char> sep(" \t");
+	  vector<string> ctx;
 
-	tokenizer<char_separator<char> > tok_id(line, sep);
-	copy(tok_id.begin(), tok_id.end(), back_inserter(ctx));
-	if (ctx.size() == 0) return is; // blank line or EOF
-	cs_id = ctx[0];
-	vector<string>().swap(ctx);
-	// next comes the context
-	if(!read_line_noblank(is, line, l_n)) return is;
+	  tokenizer<char_separator<char> > tok_id(line, sep);
+	  copy(tok_id.begin(), tok_id.end(), back_inserter(ctx));
+	  if (ctx.size() == 0) return is; // blank line or EOF
+	  cs_id = ctx[0];
+	  vector<string>().swap(ctx);
+	  // next comes the context
+	  if(!read_line_noblank(is, line, l_n)) return is;
 
-	tokenizer<char_separator<char> > tok_ctx(line, sep);
-	copy(tok_ctx.begin(), tok_ctx.end(), back_inserter(ctx));
-	if (ctx.size() == 0) return is; // blank line or EOF
-	int i = 0;
-	try {
-	  for(vector<string>::const_iterator it = ctx.begin(); it != ctx.end(); ++i, ++it) {
+	  tokenizer<char_separator<char> > tok_ctx(line, sep);
+	  copy(tok_ctx.begin(), tok_ctx.end(), back_inserter(ctx));
+	  if (ctx.size() == 0) return is; // blank line or EOF
+	  int i = 0;
+	  try {
+		for(vector<string>::const_iterator it = ctx.begin(); it != ctx.end(); ++i, ++it) {
 
-		ctw_parse_t ctwp = parse_ctw(*it);
-		if (ctwp.lemma.size() == 0) continue;
-		char pos(0);
-		CWord::cwtype cw_type = cast_int_cwtype(ctwp.dist);
-		if (cw_type == CWord::cw_error) {
-		  throw std::runtime_error(*it + " fourth field is invalid.");
-		}
-		if (cw_type != CWord::cw_concept && glVars::input::filter_pos) {
-		  if (!ctwp.pos.size()) throw std::runtime_error(*it + " has no POS.");
-		  if (ctwp.pos.size() != 1) throw std::runtime_error(*it + " has invalid POS (more than 1 char).");
-		  pos = ctwp.pos[0];
-		}
-		CWord new_cw(ctwp.lemma, ctwp.id, pos, cw_type, ctwp.w);
+		  try {
+			ctw_parse_t ctwp = parse_ctw(*it);
+			if (ctwp.lemma.size() == 0) continue;
+			char pos(0);
+			CWord::cwtype cw_type = cast_int_cwtype(ctwp.dist);
+			if (cw_type == CWord::cw_error) {
+			  throw std::runtime_error(*it + " fourth field is invalid.");
+			}
+			if (cw_type != CWord::cw_concept && glVars::input::filter_pos) {
+			  if (!ctwp.pos.size()) throw std::runtime_error(*it + " has no POS.");
+			  if (ctwp.pos.size() != 1) throw std::runtime_error(*it + " has invalid POS (more than 1 char).");
+			  pos = ctwp.pos[0];
+			}
+			CWord new_cw(ctwp.lemma, ctwp.id, pos, cw_type, ctwp.w);
 
-		if (new_cw.size()) {
-		  v.push_back(new_cw);
-		} else {
-		  // No synset for that word.
-		  if (glVars::debug::warning) {
-			cerr << "W: " << ctwp.lemma;
-			if (glVars::input::filter_pos && pos)
-			  cerr << "-" << pos;
-			cerr << " can't be mapped to KB." << endl;
+			if (new_cw.size()) {
+			  v.push_back(new_cw);
+			} else {
+			  // No synset for that word.
+			  throw std::runtime_error(*it + " can't be mapped to KB.");
+			}
+		  } catch (std::exception & e) {
+			string msg(e.what());
+			if (glVars::input::swallow) {
+			  if (glVars::debug::warning) {
+				cerr << msg << "\n";
+			  }
+			} else {
+			  throw std::runtime_error(msg);
+			}
 		  }
 		}
+	  } catch (std::exception & e) {
+		throw std::runtime_error("Context error in line " + lexical_cast<string>(l_n) + "\n" + e.what());
 	  }
-	} catch (std::exception & e) {
-	  throw std::runtime_error("Context error in line " + lexical_cast<string>(l_n) + "\n" + e.what() );
 	}
-  }
-  return is;
+	return is;
   }
 
 
@@ -815,7 +821,7 @@ namespace ukb {
   //
 
   void disamb_csentence_kb(CSentence & cs,
-							const vector<float> & ranks) {
+						   const vector<float> & ranks) {
 
 	Kb & kb = ukb::Kb::instance();
 

ukb_wsd.cc
@@ -288,6 +288,7 @@ int main(int argc, char *argv[]) {
     ("concept_graph,G", "Graph is built using just concepts. Words are no more part of the graph.")
 	("nopos", "Don't filter words by Part of Speech.")
 	("poslightw", "Light words instead of wpos when calculating personalization vector.")
+	("minput", "Do not die when dealing with malformed input.")
     ;
 
   options_description po_desc_wsd("WSD methods");
@@ -379,6 +380,10 @@ int main(int argc, char *argv[]) {
 
     if (vm.count("poslightw")) {
 	  glVars::prank::lightw = true;
+    }
+
+    if (vm.count("minput")) {
+	  glVars::input::swallow = true;
     }
 
     if (vm.count("ppr")) {

globalVars.cc
@@ -54,6 +54,7 @@ namespace ukb {
 
 	namespace input {
 	  bool filter_pos = true;
+	  bool swallow = false;
 	}
 
 	namespace output {

globalVars.h
@@ -59,6 +59,7 @@ namespace ukb {
 	  // to the KB. It also has effects on dictionary.
 
 	  extern bool filter_pos;
+	  extern bool swallow;        // tries to swallow malformed input
 	}
 
 

ukb_ppv.cc
@@ -287,6 +287,7 @@ int main(int argc, char *argv[]) {
     ("verbose,v", "Be verbose.")
 	("nopos", "Don't filter words by Part of Speech.")
 	("poslightw", "Light words instead of wpos when calculating personalization vector.")
+	("minput", "Do not die when dealing with malformed input.")
 	("wiki", "Usual options for wikipedia (sets --nopos, --only_ctx_words and --only_synsets).")
     ;
 
@@ -367,6 +368,10 @@ int main(int argc, char *argv[]) {
 
     if (vm.count("poslightw")) {
 	  glVars::prank::lightw = true;
+    }
+
+    if (vm.count("minput")) {
+	  glVars::input::swallow = true;
     }
 
     if (vm.count("only_words")) {

commit 1a43bee454f9e677e81aa58673f9c5569d3da73d

Fix bug introduced in 501d414e4a23a345d802126b3f7bde3e0403fc45

csentence.cc
@@ -432,7 +432,8 @@ namespace ukb {
 			  v.push_back(new_cw);
 			} else {
 			  // No synset for that word.
-			  throw std::runtime_error(*it + " can't be mapped to KB.");
+			  if (glVars::debug::warning)
+				cerr << "W:" << *it << " can't be mapped to KB.";
 			}
 		  } catch (std::exception & e) {
 			string msg(e.what());

ukb_wsd.cc
@@ -626,7 +626,7 @@ int main(int argc, char *argv[]) {
 	};
   }
   catch (std::exception & e) {
-    cerr << "Errore reading " << fullname_in << " : " << e.what() << "\n";
+    cerr << "Error reading " << fullname_in << " : " << e.what() << "\n";
     exit(-1);
   }
  END: